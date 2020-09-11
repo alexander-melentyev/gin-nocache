82beb1b2a1a3cae5643f1a872e5f26cef075962b
alexander-melentyev <asmelentyev@yandex.ru>
2020-09-11 08:16:16 +0300

    fix: Fixed vars

--- nocache_test.go
+++ nocache_test.go
@@ -11,14 +11,15 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-var epoch = time.Unix(0, 0).Format(time.RFC1123)
-
-var noCacheHeaders = map[string]string{
-	"Expires":         epoch,
-	"Cache-Control":   "no-cache, no-store, no-transform, must-revalidate, private, max-age=0",
-	"Pragma":          "no-cache",
-	"X-Accel-Expires": "0",
-}
+var (
+	epoch          = time.Unix(0, 0).Format(time.RFC1123)
+	noCacheHeaders = map[string]string{
+		"Expires":         epoch,
+		"Cache-Control":   "no-cache, no-store, no-transform, must-revalidate, private, max-age=0",
+		"Pragma":          "no-cache",
+		"X-Accel-Expires": "0",
+	}
+)
 
 func TestNoCache(t *testing.T) {
 	w := httptest.NewRecorder()
