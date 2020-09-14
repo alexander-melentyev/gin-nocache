077f3ffee9cff471aaa7fa41c77cf6379aef70a1
alexander-melentyev <asmelentyev@yandex.ru>
2020-09-14 13:37:20 +0300

    fix: Moved vars to out of func and added false positive

--- nocache.go
+++ nocache.go
@@ -6,6 +6,31 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+var (
+	// Unix epoch time.
+	// nolint:gochecknoglobals
+	epoch = time.Unix(0, 0).Format(time.RFC1123)
+
+	// Taken from https://github.com/mytrile/nocache
+	// nolint:gochecknoglobals
+	noCacheHeaders = map[string]string{
+		"Expires":         epoch,
+		"Cache-Control":   "no-cache, no-store, no-transform, must-revalidate, private, max-age=0",
+		"Pragma":          "no-cache",
+		"X-Accel-Expires": "0",
+	}
+
+	// nolint:gochecknoglobals
+	etagHeaders = []string{
+		"ETag",
+		"If-Modified-Since",
+		"If-Match",
+		"If-None-Match",
+		"If-Range",
+		"If-Unmodified-Since",
+	}
+)
+
 // NoCache is a simple piece of middleware that sets a number of HTTP headers to prevent
 // a router (or subrouter) from being cached by an upstream proxy and/or client.
 //
@@ -15,28 +40,6 @@ import (
 //      X-Accel-Expires: 0
 //      Pragma: no-cache (for HTTP/1.0 proxies/clients)
 func NoCache() gin.HandlerFunc {
-	var (
-		// Unix epoch time.
-		epoch = time.Unix(0, 0).Format(time.RFC1123)
-
-		// Taken from https://github.com/mytrile/nocache
-		noCacheHeaders = map[string]string{
-			"Expires":         epoch,
-			"Cache-Control":   "no-cache, no-store, no-transform, must-revalidate, private, max-age=0",
-			"Pragma":          "no-cache",
-			"X-Accel-Expires": "0",
-		}
-
-		etagHeaders = []string{
-			"ETag",
-			"If-Modified-Since",
-			"If-Match",
-			"If-None-Match",
-			"If-Range",
-			"If-Unmodified-Since",
-		}
-	)
-
 	return func(c *gin.Context) {
 		// Delete any ETag headers that may have been set
 		for _, v := range etagHeaders {
